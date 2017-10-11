8a6fc949394a0ad72b024ba9b86d34000a3c8510
Alex <9252818@mail.ru>
2017-10-11 14:19:30 +0300

    зомби

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Theory_of_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NMAX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "EPS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 64);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "C1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(132, 64);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "C2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(226, 64);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "C3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(324, 64);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "C4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 182);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 40);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(134, 40);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(228, 40);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(34, 83);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(76, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(134, 83);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(76, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(228, 83);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(76, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(322, 83);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(76, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(34, 220);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(364, 166);
+            this.textBox8.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 103);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Выбор функции";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(34, 128);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 17);
+            this.checkBox1.TabIndex = 18;
+            this.checkBox1.Text = "sqrt(x^2+y^2)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(34, 150);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 19;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(134, 128);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 21;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(134, 150);
+            this.checkBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 20;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(228, 128);
+            this.checkBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(80, 17);
+            this.checkBox5.TabIndex = 23;
+            this.checkBox5.Text = "checkBox5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(228, 150);
+            this.checkBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(80, 17);
+            this.checkBox6.TabIndex = 22;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 395);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Теория игр";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Theory_of_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public float funk(float wx, float wy)
+        {
+            //Функции выбираются из заданных ниже
+            if (checkBox1.Checked) { return (float)Math.Sqrt(wx * wx + wy * wy); }
+            if (checkBox2.Checked) { return (float)Math.Sqrt(wx * wx + wy * wy); }
+            if (checkBox3.Checked) { return (float)Math.Sqrt(wx * wx + wy * wy); }
+            if (checkBox4.Checked) { return (float)Math.Sqrt(wx * wx + wy * wy); }
+            if (checkBox5.Checked) { return (float)Math.Sqrt(wx * wx + wy * wy); }
+            if (checkBox6.Checked) { return (float)Math.Sqrt(wx * wx + wy * wy); }
+            return 0;
+        }
+
+        public float NoMarks(float wx, float wy)
+        {
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = 0, nmax = 0, c1 = 0, c2 = 0, c3 = 0, c4 = 0;
+            float eps = 0;
+            //Получение значений n,nps,eps,c1,c2,c3,c4 из текстбоксов
+            n = int.Parse(textBox1.Text);
+            nmax = int.Parse(textBox2.Text);
+            eps = float.Parse(textBox3.Text);
+            c1 = int.Parse(textBox4.Text);
+            c2 = int.Parse(textBox5.Text);
+            c3 = int.Parse(textBox6.Text);
+            c4 = int.Parse(textBox7.Text);
+            //Объявление основных используемых переменных
+            float[] x = new float[n];
+            float[] x1 = new float[n];
+            float[] y = new float[n];
+            float[] y1 = new float[n];
+            float[] pc = new float[n];
+            float[] fi = new float[n];
+            float[] x2 = new float[n];
+            float[] y2 = new float[n];
+            float[] d = new float[n];
+            float[,] z1 = new float[n,9];
+            float[,] a1 = new float[n,n];
+            int ai = 1, ai1 = 0, ai2 = 0, ai3 = 0, ai4 = 0;
+            float dx = (c2 - c1) / (n - 1), dy = (c4 - c3) / (n - 1);
+            int i3=-1, i4;
+
+            for (int i=0; i < n; i++)
+            {
+                for (int j =0; j < n; j++) {a1[i,j]=funk(c1 + (c2 - c1) * (i - 1), c3 + (c4 - c3) * (j - 1)); } //функция выбирается через checkbox
+            }
+            // метка 57 (Собираем массив А)
+            float omax = 1000000;
+            float omin = 0;
+
+            bool repit = true; //baka! Не юзай метки.
+            for (; repit;)
+            {
+                int k = 1;//понадобится в выводе ответа (i)
+
+                for (int i = 1; i < n; i++) // I1
+                {
+                    pc[i] = pc[i] + a1[i, (n + 1) / 2];
+                    if (pc[i] <= pc[k]) k = i;//уточнить условие
+                }//метка 1
+
+                x[k] = x[k] + 1;
+
+                int g = 1;//понадобится в дальнейшем (J)
+
+                for (int i = 1; i < n; i++)// J1
+                {
+                    fi[i] = fi[i] + a1[k, i];
+                    if (fi[i] <= fi[g]) g = i;//уточнить условие
+                } //метка 3
+
+                y[g] = y[g] + 1;
+
+                float omax1 = pc[k] / ai;
+                float omin1 = fi[g] / ai;
+                float c, r;
+                if ((omax - omax1) < 0)
+                {
+                    omax = omax1; ai1 = ai;
+                    ai3++;
+                    c = 0; r = 0;
+                    for (int i = 1; i < n; i++) //(i2)
+                    {
+                        x1[i] = x[i] / ai;
+                        d[i] = Math.Abs(x1[i] - x2[i]);
+                        c += d[i]; x2[i] = x1[i];
+
+                        if (d[i] > d[(int)r]) r = i;
+                    }
+                    //метка 7
+
+
+                    for (int i = 2; i < n; i++) //(i2)
+                    {
+                        i3 = n + 1 - i;
+                        i4 = i3 - 1;
+                        z1[i3, 5] = z1[i4, 5];
+                        z1[i3, 6] = z1[i4, 6];
+                    }
+                    //метка 71
+                    z1[1, 5] = c;
+                    z1[1, 6] = d[(int)r];
+                }
+                //метка 8
+                if ((omin - omin1) < 0)
+                {
+                    omin = omin1;
+                    ai2 = ai; ai4++;
+                    c = 0; r = 1;
+                    for (int i = 1; i < n; i++) //(i2)
+                    {
+                        y1[i] = y[i] / ai;
+                        d[i] = Math.Abs(y1[i] - y2[i]);
+                        c += d[i]; y2[i] = y1[i];
+                        if (d[i] > d[(int)r]) r = i;
+                    } //метка 10
+
+                    for (int i = 2; i < n; i++) //(i2)
+                    {
+                        i3 = n + 1 - i;
+                        i4 = i3 - 1;
+                        z1[i3, 7] = z1[i4, 7];
+                        z1[i3, 8] = z1[i4, 8];
+                    }
+                    //метка 73
+                    z1[1, 7] = c;
+                    z1[1, 8] = d[(int)r];
+                }
+                ai++;
+                if (ai > nmax)
+                {
+                    if (Math.Abs(omax - omin) > eps)
+                    {
+                        repit = false;
+                    }
+                }
+            }
+                //печать i3,ai,ai2,ai3,ai4
+                float F = (omax + omin) / 2;
+                //печать f, omax, omin
+                for(int i = 1; i < n; i++)
+                {
+                    z1[i, 1] = x1[i];
+                    z1[i, 2] = c4 + (i - 1) * dy;
+                    z1[i, 3] = y1[i];
+                    z1[i, 4] = c2 + (i - 1) * dx;
+                }
+            //вывод констант 
+            textBox8.Multiline = true;
+            textBox8.Text += "Вычисленные значения:" + Environment.NewLine;
+            textBox8.Text += "i3 = " + i3 +  "; ai = " + ai + "; ai1 = " + ai1 + "; ai2 = " + ai2 + "; ai3 = " + ai3 + "; ai4 = " + ai4 + Environment.NewLine;
+            textBox8.Text += "f = " + F + "; omax" + omax + "; omin = " + omin +  Environment.NewLine;
+            //Вывод массива
+            textBox8.Text += "Полученная матрица:" + Environment.NewLine;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    textBox8.Text += z1[i, j] + " ";
+                }
+                    textBox8.Text += Environment.NewLine;
+            }
+            
+        }
+    }
+}
